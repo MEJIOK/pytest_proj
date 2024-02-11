95e20a576643ef93bfb263fdc4357737a3260eee
Evgeniy Erokhov <evgeniy.erokhov@gmail.com>
2024-02-12 02:30:46 +0300

    added 100% pytest coverage

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,11 +1,18 @@
 from utils import arrs
+import pytest
 
 
 def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
+    assert arrs.get([1, 2, 3], 1, "test") == 2
     assert arrs.get([], 0, "test") == "test"
 
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([]) == []
+    assert arrs.my_slice([1, 2, 3], None, 3) == [1, 2, 3]
+
+
+if __name__ == '__main__':
+    pytest.main(["-vv"])
